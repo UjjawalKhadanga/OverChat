5c7a5c9b5c2e9d4c6c09d70ac0d9b38bda517b7b
Ujjawal Khadanga <b20139#students.iitmandi.ac.in>
2024-01-11 23:20:20 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({credentials: true, origin: process.env.CLIENT_URL}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(cookieParser());
 
 // Database Connection
