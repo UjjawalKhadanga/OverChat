35415e9bc67d11c31ac4fc304b57fe00363051d3
Ujjawal Khadanga <b20139@students.iitmandi.ac.in>
2024-01-21 17:33:07 +0530

    update react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import LoginPage from './pages/LoginPage';
@@ -6,18 +6,15 @@ import RegisterPage from './pages/RegisterPage';
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 
+const router = createBrowserRouter([
+  { path: '/', Component: LoginPage },
+  { path: '/register', Component: RegisterPage },
+  { path: '/home', Component: Home },
+  { path: '/chat/:roomId', Component: Chat },
+])
 
 function App() {
-  return (
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/chat/:roomId" element={<Chat/>} />
-        </Routes>
-      </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { SocketProvider } from './providers/socket';
 import App from './App';
 
 ReactDOM.render(
   <SocketProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </SocketProvider>,
   document.getElementById('root')
 );
